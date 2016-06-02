52bf07d04fb3a337da3021ec22f439ef7b7fedc8
Sebastian <unisorsebastian@gmail.com>
2016-06-03 02:56:03 +0300

    update sh

--- deploySmartHome.sh
+++ deploySmartHome.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #cd /home/sebastian/workspace/home-automation
-echo 'stop tomcat'
+echo "make sure you got latest code - git pull"
+echo "execute mvn clean install "
+echo 'stopping tomcat8'
 service tomcat8 stop
-echo 'remove tomcat files'
+echo 'remove tomcat files and copy new WAR'
 rm -rf /var/lib/tomcat8/logs/*
 rm -rf /var/lib/tomcat8/webapps/SmartHo*
 cp /home/sebastian/workspace/home-automation/target/SmartHome.war /var/lib/tomcat8/webapps/SmartHome.war
 #service tomcat8 start
-
+echo "please -> service tomcat8 start"
 
 
 
