a859ce6adb73ee481126cc63f9b6371a977538af
Sebastian <unisorsebastian@gmail.com>
2016-06-05 23:17:27 +0300

    remove security

--- src/main/java/ro/jmind/config/RootConfiguration.java
+++
@@ -1,9 +0,0 @@
-package ro.jmind.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan
-public class RootConfiguration {
-}
