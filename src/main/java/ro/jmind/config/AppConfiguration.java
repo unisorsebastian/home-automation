035c7d5aa69a5261f42eb26b7667db10aed1ae8c
Sebastian Unisor <sebastian.unisor@1and1.ro>
2016-06-07 19:25:51 +0300

    add spring security

---
+++ src/main/java/ro/jmind/config/AppConfiguration.java
@@ -0,0 +1,26 @@
+package ro.jmind.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "ro.jmind")
+public class AppConfiguration {
+     
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/view/jsp/");
+        viewResolver.setSuffix(".jsp");
+ 
+        return viewResolver;
+    }
+ 
+}
